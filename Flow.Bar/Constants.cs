b3eeb3ca39893983d65d829d9b846fae1224714c
Jack251970 <1160210343@qq.com>
2025-07-26 16:30:53 +0800

    Add more constants

--- Flow.Bar/Constants.cs
+++ Flow.Bar/Constants.cs
@@ -1,6 +1,37 @@
-﻿namespace Flow.Bar;
+﻿using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+
+namespace Flow.Bar;
 
 public static class Constants
 {
     public const string FlowBar = "Flow.Bar";
+    public const string FlowBarFullName = "Flow Bar";
+    public const string Plugins = "Plugins";
+    public const string PluginMetadataFileName = "plugin.json";
+
+    public const string ApplicationFileName = FlowBar + ".exe";
+
+    private static readonly Assembly Assembly = Assembly.GetExecutingAssembly();
+    public static readonly string ProgramDirectory = Directory.GetParent(Assembly.Location)!.ToString();
+    public static readonly string ExecutablePath = Path.Combine(ProgramDirectory, FlowBar + ".exe");
+    public static readonly string ApplicationDirectory = Directory.GetParent(ProgramDirectory)!.ToString();
+    public static readonly string RootDirectory = Directory.GetParent(ApplicationDirectory)!.ToString();
+
+    public static readonly string PreinstalledDirectory = Path.Combine(ProgramDirectory, Plugins);
+    public const string IssuesUrl = "https://github.com/Flow-Launcher/Flow.Launcher/issues";
+    public static readonly string Version = FileVersionInfo.GetVersionInfo(Assembly.Location).ProductVersion!;
+    public static readonly string Dev = "Dev";
+    public const string Documentation = "https://flowlauncher.com/docs/#/usage-tips";
+
+    private static readonly string ImagesDirectory = Path.Combine(ProgramDirectory, "Images");
+    public static readonly string DefaultIcon = Path.Combine(ImagesDirectory, "app.png");
+
+    public const string Themes = "Themes";
+    public const string Settings = "Settings";
+    public const string Logs = "Logs";
+    public const string Cache = "Cache";
+
+    public const string SystemLanguageCode = "system";
 }
---
+++ Flow.Bar/Extensions/Data/DataLocation.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+
+namespace Flow.Bar.Extensions.Data;
+
+public static class DataLocation
+{
+    public const string PortableFolderName = "UserData";
+    public const string DeletionIndicatorFile = ".dead";
+    public readonly static string PortableDataPath = Path.Combine(Constants.ProgramDirectory, PortableFolderName);
+    public readonly static string RoamingDataPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "FlowLauncher");
+    public static string DataDirectory() =>
+        PortableDataLocationInUse() ? PortableDataPath : RoamingDataPath;
+
+    public static bool PortableDataLocationInUse() =>
+        Directory.Exists(PortableDataPath) && !File.Exists(DeletionIndicatorFile);
+
+    public static string VersionLogDirectory => Path.Combine(LogDirectory, Constants.Version);
+    public static string LogDirectory => Path.Combine(DataDirectory(), Constants.Logs);
+
+    public static readonly string CacheDirectory = Path.Combine(DataDirectory(), Constants.Cache);
+    public static readonly string SettingsDirectory = Path.Combine(DataDirectory(), Constants.Settings);
+    public static readonly string PluginsDirectory = Path.Combine(DataDirectory(), Constants.Plugins);
+    public static readonly string ThemesDirectory = Path.Combine(DataDirectory(), Constants.Themes);
+
+    public static readonly string PluginSettingsDirectory = Path.Combine(SettingsDirectory, Constants.Plugins);
+    public static readonly string PluginCacheDirectory = Path.Combine(DataDirectory(), Constants.Cache, Constants.Plugins);
+}
