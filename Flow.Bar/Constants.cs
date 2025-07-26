60a9a6731b6b8f2a5eb80f841f183472e420d996
Jack251970 <1160210343@qq.com>
2025-07-26 16:43:14 +0800

    Fix constants issue

--- Flow.Bar/Constants.cs
+++ Flow.Bar/Constants.cs
@@ -6,7 +6,7 @@ namespace Flow.Bar;
 
 public static class Constants
 {
-    public const string FlowBar = "Flow.Bar";
+    public const string FlowBar = "FlowBar";
     public const string FlowBarFullName = "Flow Bar";
     public const string Plugins = "Plugins";
     public const string PluginMetadataFileName = "plugin.json";
@@ -20,10 +20,9 @@ public static class Constants
     public static readonly string RootDirectory = Directory.GetParent(ApplicationDirectory)!.ToString();
 
     public static readonly string PreinstalledDirectory = Path.Combine(ProgramDirectory, Plugins);
-    public const string IssuesUrl = "https://github.com/Flow-Launcher/Flow.Launcher/issues";
+    public const string IssuesUrl = "https://github.com/Flow-Bar/Flow.Bar/issues";
     public static readonly string Version = FileVersionInfo.GetVersionInfo(Assembly.Location).ProductVersion!;
     public static readonly string Dev = "Dev";
-    public const string Documentation = "https://flowlauncher.com/docs/#/usage-tips";
 
     private static readonly string ImagesDirectory = Path.Combine(ProgramDirectory, "Images");
     public static readonly string DefaultIcon = Path.Combine(ImagesDirectory, "app.png");
