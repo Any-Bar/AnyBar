e99cb66dddafde6ad12f7cb9c57b39f881758fed
Jack251970 <1160210343@qq.com>
2025-07-26 17:15:59 +0800

    Add win32 helper

---
+++ Flow.Bar/Helper/PInvoke/Win32Helper.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+using System.Windows.Interop;
+using Windows.Win32;
+using Windows.Win32.Foundation;
+
+namespace Flow.Bar.Helper;
+
+public class Win32Helper
+{
+    public static bool SetForegroundWindow(Window window)
+    {
+        return PInvoke.SetForegroundWindow(GetWindowHandle(window));
+    }
+
+    public static bool SetForegroundWindow(nint handle)
+    {
+        return PInvoke.SetForegroundWindow(new(handle));
+    }
+
+    internal static HWND GetWindowHandle(Window window, bool ensure = false)
+    {
+        var windowHelper = new WindowInteropHelper(window);
+        if (ensure)
+        {
+            windowHelper.EnsureHandle();
+        }
+        return new(windowHelper.Handle);
+    }
+}
