cdf36b6e6520bd15f839285ca0f37f67860504b0
Jack251970 <1160210343@qq.com>
2025-07-26 16:31:11 +0800

    Add settings

---
+++ Flow.Bar/Models/UserSettings/Settings.cs
@@ -0,0 +1,18 @@
+﻿using Flow.Bar.Models.Storage;
+
+namespace Flow.Bar.Models.UserSettings;
+
+public class Settings
+{
+    private FlowBarJsonStorage<Settings> _storage = null!;
+
+    public void SetStorage(FlowBarJsonStorage<Settings> storage)
+    {
+        _storage = storage;
+    }
+
+    public void Save()
+    {
+        _storage.Save();
+    }
+}
