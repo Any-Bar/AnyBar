8919e41a82b6dc31ec7263e4126411345a906bcf
Jack251970 <1160210343@qq.com>
2025-08-01 14:32:27 +0800

    Add IEquatable for AppBarModel

--- Flow.Bar/Models/AppBar/AppBarModel.cs
+++ Flow.Bar/Models/AppBar/AppBarModel.cs
@@ -1,9 +1,10 @@
 ﻿using Flow.Bar.Models.Enums;
+using System;
 using System.Collections.Generic;
 
 namespace Flow.Bar.Models.AppBar;
 
-public class AppBarModel
+public class AppBarModel : IEquatable<AppBarModel>
 {
     public int Order { get; set; } = -1;
 
@@ -20,4 +21,38 @@ public class AppBarModel
     public List<PluginControlModel> RightOrBottomPluginControls { get; set; } = [];
 
     public List<PluginControlModel> CenterPluginControls { get; set; } = [];
+
+    /// <inheritdoc />
+    public override string ToString() => $"{Order}";
+
+    /// <inheritdoc />
+    public override bool Equals(object? obj)
+    {
+        return Equals(obj as AppBarModel);
+    }
+
+    /// <inheritdoc />
+    public override int GetHashCode() => Order.GetHashCode();
+
+    /// <inheritdoc />
+    public bool Equals(AppBarModel? other) => Order == other?.Order;
+
+    /// <inheritdoc />
+    public static bool operator ==(AppBarModel? a, AppBarModel? b)
+    {
+        if (ReferenceEquals(a, b))
+        {
+            return true;
+        }
+
+        if (a is null)
+        {
+            return false;
+        }
+
+        return a.Equals(b);
+    }
+
+    /// <inheritdoc />
+    public static bool operator !=(AppBarModel? a, AppBarModel? b) => !(a == b);
 }
