9ff1ca41369443e3f3dd1fe91aae74c9a1f99ddc
Jack251970 <1160210343@qq.com>
2025-08-03 17:42:42 +0800

    Add ListViewEx

---
+++ Flow.Bar/Controls/ListView/ListViewEx.cs
@@ -0,0 +1,26 @@
+﻿using iNKORE.UI.WPF.Modern.Controls;
+using System.Windows;
+
+namespace Flow.Bar.Controls.ListView;
+
+public class ListViewEx : ListViewBase
+{
+    static ListViewEx()
+    {
+        DefaultStyleKeyProperty.OverrideMetadata(typeof(ListViewEx), new FrameworkPropertyMetadata(typeof(ListViewEx)));
+    }
+
+    public ListViewEx()
+    {
+    }
+
+    protected override bool IsItemItsOwnContainerOverride(object item)
+    {
+        return item is ListViewItem;
+    }
+
+    protected override DependencyObject GetContainerForItemOverride()
+    {
+        return new ListViewItem();
+    }
+}
---
+++ Flow.Bar/Controls/ListView/ListViewItem.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+using iNKORE.UI.WPF.Modern.Controls;
+
+namespace Flow.Bar.Controls.ListView;
+
+public class ListViewItemEx : ListViewBaseItem
+{
+    static ListViewItemEx()
+    {
+        DefaultStyleKeyProperty.OverrideMetadata(typeof(ListViewItemEx), new FrameworkPropertyMetadata(typeof(ListViewItemEx)));
+    }
+}
