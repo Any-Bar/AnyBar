d50c1e608e74ce262d7e5ef2614938aef54c02ba
Jack251970 <1160210343@qq.com>
2025-08-05 13:12:50 +0800

    Support SettingWindow draggable change

--- Flow.Bar/Views/SettingWindow.xaml.cs
+++ Flow.Bar/Views/SettingWindow.xaml.cs
@@ -2,6 +2,7 @@
 using Flow.Bar.Services;
 using System;
 using System.Windows;
+using System.Windows.Shell;
 
 namespace Flow.Bar.Views;
 
@@ -9,12 +10,29 @@ public partial class SettingWindow : Window
 {
     private readonly NavigationViewService _navigationService = Ioc.Default.GetRequiredService<NavigationViewService>();
 
+    private WindowChrome _draggableChrome = null!;
+    private WindowChrome _nonDraggableChrome = null!;
+
     public SettingWindow()
     {
         InitializeComponent();
     }
 
     #region Window Events
+
+    private void Window_Loaded(object sender, RoutedEventArgs e)
+    {
+        _draggableChrome = WindowChrome.GetWindowChrome(this);
+        _nonDraggableChrome = new WindowChrome
+        {
+            NonClientFrameEdges = _draggableChrome.NonClientFrameEdges,
+            CaptionHeight = 0,  // Disable caption height for non-draggable chrome
+            CornerRadius = _draggableChrome.CornerRadius,
+            GlassFrameThickness = _draggableChrome.GlassFrameThickness,
+            ResizeBorderThickness = _draggableChrome.ResizeBorderThickness,
+            UseAeroCaptionButtons = _draggableChrome.UseAeroCaptionButtons
+        };
+    }
 
     private void Window_Closed(object sender, EventArgs e)
     {
@@ -43,6 +61,22 @@ public partial class SettingWindow : Window
     private void RootFrame_Unloaded(object sender, RoutedEventArgs e)
     {
         _navigationService.UnregisterFrameEvents(RootFrame);
+    }
+
+    #endregion
+
+    #region Draggable
+
+    public void SetDraggable(bool isDraggable)
+    {
+        if (isDraggable)
+        {
+            WindowChrome.SetWindowChrome(this, _draggableChrome);
+        }
+        else
+        {
+            WindowChrome.SetWindowChrome(this, _nonDraggableChrome);
+        }
     }
 
     #endregion
