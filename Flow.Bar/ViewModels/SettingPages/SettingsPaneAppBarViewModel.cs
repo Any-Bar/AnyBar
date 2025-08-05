b6e4965a3bf7b4f30c146b911db749f98c9633d6
Jack251970 <1160210343@qq.com>
2025-08-05 13:21:05 +0800

    Do not allow window moving during dialog popup

--- Flow.Bar/ViewModels/SettingPages/SettingsPaneAppBarViewModel.cs
+++ Flow.Bar/ViewModels/SettingPages/SettingsPaneAppBarViewModel.cs
@@ -1,14 +1,16 @@
-﻿using System.Collections.ObjectModel;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using CommunityToolkit.Mvvm.ComponentModel;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using Flow.Bar.Controls;
 using Flow.Bar.Dialogs;
 using Flow.Bar.Interfaces;
 using Flow.Bar.Models.AppBar;
 using Flow.Bar.Services;
+using Flow.Bar.Views;
+using System;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace Flow.Bar.ViewModels.SettingPages;
 
@@ -23,11 +25,32 @@ public partial class SettingsPaneAppBarViewModel(AppBarManagementService appBarM
     [RelayCommand]
     private async Task AddAppBarAsync(Button button)
     {
-        var dialog = new AddAppBarDialog()
+        AddAppBarDialog dialog;
+        ContentDialogResult result;
+        if (Window.GetWindow(button) is SettingWindow owner)
         {
-            Owner = Window.GetWindow(button)
-        };
-        if (await dialog.ShowAsync() == ContentDialogResult.Primary)
+            try
+            {
+                dialog = new AddAppBarDialog()
+                {
+                    Owner = owner
+                };
+                owner.SetDraggable(false);
+                result = await dialog.ShowAsync();
+                owner.SetDraggable(true);
+            }
+            catch (Exception ex)
+            {
+                App.API.LogException(ClassName, "Failed to show AddAppBarDialog", ex);
+                return;
+            }
+        }
+        else
+        {
+            App.API.LogError(ClassName, "Failed to get owner window for AddAppBarDialog");
+            return;
+        }
+        if (result == ContentDialogResult.Primary)
         {
             var model = new AppBarModel
             {
