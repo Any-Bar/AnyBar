64332a9b8b3ca7cd2ac8ea68b73c97a0f2bab4c3
Jack251970 <1160210343@qq.com>
2025-08-06 09:25:17 +0800

    Add CanReorderItemsProperty & DropScrollViewerProperty to StackPanelViewEx

--- Flow.Bar/Controls/ListView/StackPanelViewEx.cs
+++ Flow.Bar/Controls/ListView/StackPanelViewEx.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using Flow.Bar.Helper.DragDrop;
+using System.Windows;
 using System.Windows.Controls;
 
 namespace Flow.Bar.Controls;
@@ -27,6 +28,60 @@ public class StackPanelViewEx : StackPanelViewExBase
     {
         get => (Orientation)GetValue(OrientationProperty);
         set => SetValue(OrientationProperty, value);
+    }
+
+    #endregion
+
+    #region CanReorderItems
+
+    public static readonly DependencyProperty CanReorderItemsProperty =
+        DependencyProperty.Register(
+            nameof(CanReorderItems),
+            typeof(bool),
+            typeof(StackPanelViewEx),
+            new FrameworkPropertyMetadata(false, OnCanReorderItemsPropertyChanged));
+
+    private static void OnCanReorderItemsPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        if ((bool)e.OldValue)
+        {
+            d.DisableDragDrop();
+        }
+        if ((bool)e.NewValue)
+        {
+            d.EnableDragDrop();
+        }
+    }
+
+    public bool CanReorderItems
+    {
+        get => (bool)GetValue(CanReorderItemsProperty);
+        set => SetValue(CanReorderItemsProperty, value);
+    }
+
+    #endregion
+
+    #region DropScrollViewer
+
+    public static readonly DependencyProperty DropScrollViewerProperty =
+        DependencyProperty.Register(
+            nameof(DropScrollViewer),
+            typeof(ScrollViewer),
+            typeof(StackPanelViewEx),
+            new FrameworkPropertyMetadata(null, OnDropScrollViewerPropertyChanged));
+
+    private static void OnDropScrollViewerPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        if (e.NewValue is ScrollViewer scrollViewer)
+        {
+            d.SetDropScrollViewer(scrollViewer);
+        }
+    }
+
+    public ScrollViewer DropScrollViewer
+    {
+        get => (ScrollViewer)GetValue(DropScrollViewerProperty);
+        set => SetValue(DropScrollViewerProperty, value);
     }
 
     #endregion
---
+++ Flow.Bar/Helper/DragDrop/DragDropHelper.cs
@@ -0,0 +1,71 @@
+﻿using iNKORE.UI.WPF.Modern;
+using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using WpfDragDrop = GongSolutions.Wpf.DragDrop.DragDrop;
+
+namespace Flow.Bar.Helper.DragDrop;
+
+public static class DragDropHelper
+{
+    private readonly static DataTemplate _emptyEffectAdornerTemplate = new();
+
+    public static void EnableDragDrop(this DependencyObject element)
+    {
+        ArgumentNullException.ThrowIfNull(element, nameof(element));
+
+        WpfDragDrop.SetIsDragSource(element, true);
+        WpfDragDrop.SetIsDropTarget(element, true);
+        WpfDragDrop.SetUseDefaultEffectDataTemplate(element, false);
+        WpfDragDrop.SetEffectAllAdornerTemplate(element, _emptyEffectAdornerTemplate);
+        WpfDragDrop.SetEffectCopyAdornerTemplate(element, _emptyEffectAdornerTemplate);
+        WpfDragDrop.SetEffectLinkAdornerTemplate(element, _emptyEffectAdornerTemplate);
+        WpfDragDrop.SetEffectMoveAdornerTemplate(element, _emptyEffectAdornerTemplate);
+        WpfDragDrop.SetEffectNoneAdornerTemplate(element, _emptyEffectAdornerTemplate);
+        WpfDragDrop.SetEffectScrollAdornerTemplate(element, _emptyEffectAdornerTemplate);
+    }
+
+    public static void SetDropScrollViewer(this DependencyObject element, ScrollViewer scrollViewer)
+    {
+        ArgumentNullException.ThrowIfNull(element, nameof(element));
+        ArgumentNullException.ThrowIfNull(scrollViewer, nameof(scrollViewer));
+
+        WpfDragDrop.SetDropTargetScrollViewer(element, scrollViewer);
+        WpfDragDrop.SetDropTargetAdornerBrush(element, System.Windows.Application.Current.TryFindResource(ThemeKeys.AccentFillColorDefaultBrushKey) as Brush);
+        var horizontalScrollBarEnabled = scrollViewer.HorizontalScrollBarVisibility == ScrollBarVisibility.Visible ||
+            scrollViewer.HorizontalScrollBarVisibility == ScrollBarVisibility.Auto;
+        var verticalScrollBarEnabled = scrollViewer.VerticalScrollBarVisibility == ScrollBarVisibility.Visible ||
+            scrollViewer.VerticalScrollBarVisibility == ScrollBarVisibility.Auto;
+        if (horizontalScrollBarEnabled)
+        {
+            if (verticalScrollBarEnabled)
+            {
+                WpfDragDrop.SetDropScrollingMode(element, GongSolutions.Wpf.DragDrop.ScrollingMode.Both);
+            }
+            else
+            {
+                WpfDragDrop.SetDropScrollingMode(element, GongSolutions.Wpf.DragDrop.ScrollingMode.HorizontalOnly);
+            }
+        }
+        else
+        {
+            if (verticalScrollBarEnabled)
+            {
+                WpfDragDrop.SetDropScrollingMode(element, GongSolutions.Wpf.DragDrop.ScrollingMode.VerticalOnly);
+            }
+            else
+            {
+                WpfDragDrop.SetDropScrollingMode(element, GongSolutions.Wpf.DragDrop.ScrollingMode.None);
+            }
+        }
+    }
+
+    public static void DisableDragDrop(this DependencyObject element)
+    {
+        ArgumentNullException.ThrowIfNull(element, nameof(element));
+
+        WpfDragDrop.SetIsDragSource(element, false);
+        WpfDragDrop.SetIsDropTarget(element, false);
+    }
+}
