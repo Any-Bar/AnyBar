effa8aebdfe00e6d3ff28255206882e233ba7909
Jack251970 <1160210343@qq.com>
2025-08-06 20:25:50 +0800

    Improve WindowTracker

--- Flow.Bar/Helper/Windows/WindowTracker.cs
+++ Flow.Bar/Helper/Windows/WindowTracker.cs
@@ -5,14 +5,27 @@ namespace Flow.Bar.Helper.Windows;
 
 public static class WindowTracker
 {
-    public static List<Window> ActiveWindows { get; } = [];
+    private readonly static List<Window> _activeWindows = [];
 
     public static void TrackWindow(Window window)
     {
         window.Closed += (sender, args) =>
         {
-            ActiveWindows.Remove(window);
+            _activeWindows.Remove(window);
         };
-        ActiveWindows.Add(window);
+        _activeWindows.Add(window);
+    }
+
+    public static List<T> GetActiveWindow<T>() where T : Window
+    {
+        var result = new List<T>();
+        foreach (var window in _activeWindows)
+        {
+            if (window is T typedWindow)
+            {
+                result.Add(typedWindow);
+            }
+        }
+        return result;
     }
 }
