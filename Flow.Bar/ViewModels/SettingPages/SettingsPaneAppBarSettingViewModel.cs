f1fa9f5104b59435c15d7b196567c6c792ce2d90
Jack251970 <1160210343@qq.com>
2025-08-09 11:08:20 +0800

    Improve code quality

--- Flow.Bar/ViewModels/SettingPages/SettingsPaneAppBarSettingViewModel.cs
+++ Flow.Bar/ViewModels/SettingPages/SettingsPaneAppBarSettingViewModel.cs
@@ -1,6 +1,5 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
-using Flow.Bar.Converters;
 using Flow.Bar.Helper.Monitor;
 using Flow.Bar.Interfaces;
 using Flow.Bar.Models.AppBar;
