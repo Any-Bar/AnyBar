ce3b4bb8660f4d35da3dbbad386e092634f972ae
Jack251970 <1160210343@qq.com>
2025-08-09 10:32:18 +0800

    Implement IPluginI18n for Flow.Bar.Plugin.Clock

--- Plugins/Flow.Bar.Plugin.Clock/Main.cs
+++ Plugins/Flow.Bar.Plugin.Clock/Main.cs
@@ -3,7 +3,7 @@ using System.Windows;
 
 namespace Flow.Bar.Plugin.Clock;
 
-public class Main : IPlugin
+public class Main : IPlugin, IPluginI18n
 {
     internal static PluginInitContext Context { get; private set; }
 
@@ -15,5 +15,15 @@ public class Main : IPlugin
     public FrameworkElement GetBarElement(BarElementPosition position)
     {
         return new ClockControl(position);
+    }
+
+    public string GetTranslatedPluginTitle()
+    {
+        return Context.API.GetTranslation("FlowBarPlugin_Clock_PluginName");
+    }
+
+    public string GetTranslatedPluginDescription()
+    {
+        return Context.API.GetTranslation("FlowBarPlugin_Clock_PluginDescription");
     }
 }
