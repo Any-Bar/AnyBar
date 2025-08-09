bfeae263d208d55ad47c65b1c2c497f787338f03
Jack251970 <1160210343@qq.com>
2025-08-09 21:37:33 +0800

    Add uninstall popup menu for MoreOptionsButton

--- Flow.Bar/Views/SettingPages/SettingsPanePlugins.xaml.cs
+++ Flow.Bar/Views/SettingPages/SettingsPanePlugins.xaml.cs
@@ -5,12 +5,17 @@ using Flow.Bar.ViewModels.SettingPages;
 using iNKORE.UI.WPF.Modern.Controls;
 using System.Windows;
 using System.Windows.Navigation;
+using MenuItem = System.Windows.Controls.MenuItem;
+using HeaderedItemsControl = System.Windows.Controls.HeaderedItemsControl;
 
 namespace Flow.Bar.Views.SettingPages;
 
 public partial class SettingsPanePlugins : Page
 {
     private SettingsPanePluginsViewModel _viewModel = null!;
+
+    private MenuFlyoutEx _contextMenu = null!;
+    private PluginViewModel? _contextMenuPlugin = null;
 
     protected override void OnNavigatedTo(NavigationEventArgs e)
     {
@@ -20,6 +25,21 @@ public partial class SettingsPanePlugins : Page
             _viewModel = Ioc.Default.GetRequiredService<SettingsPanePluginsViewModel>();
             DataContext = _viewModel;
         }
+        if (_contextMenu == null)
+        {
+            _contextMenu = new();
+            var settingItem = new MenuItem();
+            settingItem.SetResourceReference(HeaderedItemsControl.HeaderProperty, nameof(Localize.SettingPanePlugins_Uninstall));
+            settingItem.Click += (o, e) =>
+            {
+                if (_contextMenuPlugin != null)
+                {
+                    // TODO
+                }
+            };
+            _contextMenu.Items.Add(settingItem);
+            _contextMenu.Closed += ContextMenu_Closed;
+        }
         if (!IsInitialized)
         {
             InitializeComponent();
@@ -27,10 +47,19 @@ public partial class SettingsPanePlugins : Page
         base.OnNavigatedTo(e);
     }
 
+    private void ContextMenu_Closed(object? sender, object? e)
+    {
+        _contextMenuPlugin = null;
+    }
+
     private void MoreOptionsButton_Click(object sender, RoutedEventArgs e)
     {
         if (sender is not FontIconButton button) return;
         if (button.Tag is not PluginViewModel plugin) return;
-
+        _contextMenuPlugin = plugin;
+        _contextMenu.ShowAt(button, new MenuFlyoutExOptions()
+        {
+            Placement = MenuFlyoutExPlacementMode.BottomEdgeAlignedRight
+        });
     }
 }
