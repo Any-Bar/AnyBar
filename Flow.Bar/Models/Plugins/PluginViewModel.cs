886627ea99687cee05751012548460770f2554b1
Jack251970 <1160210343@qq.com>
2025-08-09 12:43:26 +0800

    Add PluginViewModel

---
+++ Flow.Bar/Models/Plugins/PluginViewModel.cs
@@ -0,0 +1,56 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Flow.Bar.Helper.Image;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace Flow.Bar.Models.Plugins;
+
+public partial class PluginViewModel : ObservableObject
+{
+    public PluginPair PluginPair { get; }
+
+    public string ID { get; }
+
+    public PluginViewModel(PluginPair pluginPair)
+    {
+        PluginPair = pluginPair;
+        ID = pluginPair.Metadata.ID;
+        Name = pluginPair.Metadata.Name;
+        Description = pluginPair.Metadata.Description;
+        Version = pluginPair.Metadata.Version;
+        Author = pluginPair.Metadata.Author;
+    }
+
+    private ImageSource _image = ImageLoader.MissingImage;
+    public ImageSource Image
+    {
+        get
+        {
+            if (_image == ImageLoader.MissingImage)
+            {
+                _ = LoadIconAsync();
+            }
+
+            return _image;
+        }
+        set => _image = value;
+    }
+
+    private async Task LoadIconAsync()
+    {
+        Image = await App.API.LoadImageAsync(PluginPair.Metadata.IcoPath);
+        OnPropertyChanged(nameof(Image));
+    }
+
+    [ObservableProperty]
+    private string _name = string.Empty;
+
+    [ObservableProperty]
+    private string _description = string.Empty;
+
+    [ObservableProperty]
+    private string _version = string.Empty;
+
+    [ObservableProperty]
+    private string _author = string.Empty;
+}
