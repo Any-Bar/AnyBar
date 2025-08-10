b90d090a7137c1174ef92153ad2ef091e09bc20b
Jack251970 <1160210343@qq.com>
2025-08-10 15:34:49 +0800

    Add EnumerableExtension

---
+++ Flow.Bar/Extensions/Enumerable/EnumerableExtension.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.ObjectModel;
+
+namespace Flow.Bar.Extensions.Enumerable;
+
+public static class EnumerableExtension
+{
+    public static int RemoveAll<T>(this ObservableCollection<T> collection, Predicate<T> match)
+    {
+        ArgumentNullException.ThrowIfNull(collection);
+        ArgumentNullException.ThrowIfNull(match);
+
+        var removedCount = 0;
+        for (var i = collection.Count - 1; i >= 0; i--)
+        {
+            if (match(collection[i]))
+            {
+                collection.RemoveAt(i);
+                removedCount++;
+            }
+        }
+        return removedCount;
+    }
+}
