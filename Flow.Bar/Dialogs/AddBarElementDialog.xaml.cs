74ddf550bf4d2d41211225d27a891ae067f8d204
Jack251970 <1160210343@qq.com>
2025-08-10 20:55:51 +0800

    Add AddBarElementDialog

---
+++ Flow.Bar/Dialogs/AddBarElementDialog.xaml.cs
@@ -0,0 +1,39 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Flow.Bar.Controls;
+using Flow.Bar.Helper.Plugins;
+using Flow.Bar.Models.Plugins;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace Flow.Bar.Dialogs;
+
+[INotifyPropertyChanged]
+public partial class AddBarElementDialog : ContentDialog
+{
+    [ObservableProperty]
+    private string _searchText = string.Empty;
+
+    [ObservableProperty]
+    private PluginViewModel? _plugin;
+
+    public ObservableCollection<PluginViewModel> AllPlugins { get; } = [];
+
+    private readonly List<PluginViewModel> _allPlugins;
+
+    public AddBarElementDialog()
+    {
+        InitializeComponent();
+        _allPlugins = [.. PluginManager.AllPlugins.Select(plugin => new PluginViewModel(plugin)).OrderBy(x => x.Name)];
+        RefreshPlugins();
+    }
+
+    private void RefreshPlugins()
+    {
+        AllPlugins.Clear();
+        foreach (var plugin in _allPlugins)
+        {
+            AllPlugins.Add(plugin);
+        }
+    }
+}
