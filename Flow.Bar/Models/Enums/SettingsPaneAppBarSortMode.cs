1fc9a54a4f3eca8c9d45186d64d1c98c5c29f164
Jack251970 <1160210343@qq.com>
2025-08-11 18:29:38 +0800

    Add new sort mode

---
+++ Flow.Bar/Models/Enums/SettingsPaneAppBarSortMode.cs
@@ -0,0 +1,16 @@
+﻿using Flow.Bar.Localization.Attributes;
+
+namespace Flow.Bar.Models.Enums;
+
+[EnumLocalize]
+public enum SettingsPaneAppBarSortMode
+{
+    [EnumLocalizeKey(nameof(Localize.SettingsPaneAppBarSortMode_Order))]
+    Order,
+
+    [EnumLocalizeKey(nameof(Localize.SettingPanePluginsSortMode_Status))]
+    Status,
+
+    [EnumLocalizeKey(nameof(Localize.SettingsPaneAppBarSortMode_Name))]
+    Name,
+}
---
+++ Flow.Bar/Models/Enums/SettingsPaneBarElementSettingSortMode.cs
@@ -0,0 +1,35 @@
+﻿using Flow.Bar.Localization.Attributes;
+
+namespace Flow.Bar.Models.Enums;
+
+[EnumLocalize]
+public enum SettingsPaneBarElementSettingHorizontalSortMode
+{
+    [EnumLocalizeKey(nameof(Localize.SettingsPaneBarElementSettingSortMode_LeftToRight))]
+    LeftToRight,
+
+    [EnumLocalizeKey(nameof(Localize.SettingsPaneBarElementSettingSortMode_RightToLeft))]
+    RightToLeft,
+
+    [EnumLocalizeKey(nameof(Localize.SettingPanePluginsSortMode_Status))]
+    Status,
+
+    [EnumLocalizeKey(nameof(Localize.SettingsPaneAppBarSortMode_Name))]
+    Name,
+}
+
+[EnumLocalize]
+public enum SettingsPaneBarElementSettingVerticalSortMode
+{
+    [EnumLocalizeKey(nameof(Localize.SettingsPaneBarElementSettingSortMode_TopToBottom))]
+    TopToBottom,
+
+    [EnumLocalizeKey(nameof(Localize.SettingsPaneBarElementSettingSortMode_BottomToTop))]
+    BottomToTop,
+
+    [EnumLocalizeKey(nameof(Localize.SettingPanePluginsSortMode_Status))]
+    Status,
+
+    [EnumLocalizeKey(nameof(Localize.SettingsPaneAppBarSortMode_Name))]
+    Name,
+}
