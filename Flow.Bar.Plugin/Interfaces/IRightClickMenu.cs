5c864cfac3b26990bf66b85cb11864efcc7b7840
Jack251970 <1160210343@qq.com>
2025-08-12 17:00:05 +0800

    Add IRightClickMenu

---
+++ Flow.Bar.Plugin/Interfaces/IRightClickMenu.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Windows.Controls;
+
+namespace Flow.Bar.Plugin;
+
+/// <summary>
+/// Interface for right-click context menus in Flow Bar plugins.
+/// </summary>
+public interface IRightClickMenu
+{
+    /// <summary>
+    /// Retrieves a list of menu items for the right-click context menu.
+    /// </summary>
+    /// <returns></returns>
+    public IList<MenuItem> GetRightClickMenuItems();
+}
