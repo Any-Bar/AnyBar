204179f07aae434cd4fa42886cae3cb5f3535ecd
Jack251970 <1160210343@qq.com>
2025-08-12 16:41:22 +0800

    Add StackViewItemMouseButtonEventArgs.OriginalEventArgs

--- Flow.Bar/Controls/StackView/StackViewItemMouseButtonEventArgs.cs
+++ Flow.Bar/Controls/StackView/StackViewItemMouseButtonEventArgs.cs
@@ -1,12 +1,15 @@
-﻿using System.Windows.Input;
+﻿using System.Windows;
+using System.Windows.Input;
 
 namespace Flow.Bar.Controls;
 
 public delegate void StackViewItemMouseButtonEventHandler(object sender, StackViewItemMouseButtonEventArgs e);
 
-public sealed class StackViewItemMouseButtonEventArgs(object item, MouseButtonEventArgs e) : MouseButtonEventArgs(e.MouseDevice, e.Timestamp, e.ChangedButton, e.StylusDevice)
+public sealed class StackViewItemMouseButtonEventArgs(object item, MouseButtonEventArgs e) : RoutedEventArgs
 {
-    public new int ClickCount { get; internal set; } = e.ClickCount;
+    public int ClickCount { get; internal set; } = e.ClickCount;
 
     public object ClickedItem { get; internal set; } = item;
+
+    public MouseButtonEventArgs OriginalEventArgs { get; } = e;
 }
