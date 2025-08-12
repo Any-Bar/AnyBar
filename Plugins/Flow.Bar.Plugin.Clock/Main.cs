b2de7ffd42f40034bbe9ec55ef6330ef11bfaac1
Jack251970 <1160210343@qq.com>
2025-08-12 17:08:57 +0800

    Implement IRightClickMenu for Flow.Bar.Plugin.Clock

--- Plugins/Flow.Bar.Plugin.Clock/Main.cs
+++ Plugins/Flow.Bar.Plugin.Clock/Main.cs
@@ -1,9 +1,13 @@
 using Flow.Bar.Plugin.Clock.Views;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.Windows;
+using System.Windows.Controls;
 
 namespace Flow.Bar.Plugin.Clock;
 
-public class Main : IPlugin, IPluginI18n
+public class Main : IPlugin, IPluginI18n, IRightClickMenu
 {
     internal static PluginInitContext Context { get; private set; }
 
@@ -25,5 +29,33 @@ public class Main : IPlugin, IPluginI18n
     public string GetTranslatedPluginDescription()
     {
         return Context.API.GetTranslation("FlowBarPlugin_Clock_PluginDescription");
+    }
+
+    public IList<MenuItem> GetRightClickMenuItems()
+    {
+        var menuItems = new List<MenuItem>();
+        var adjustDateTimeItem = new MenuItem();
+        adjustDateTimeItem.SetResourceReference(HeaderedItemsControl.HeaderProperty, "FlowBarPlugin_Clock_RightClickMenu_AdjustDateTime");
+        adjustDateTimeItem.Click += AdjustDateTimeItem_Click;
+        menuItems.Add(adjustDateTimeItem);
+        return menuItems;
+    }
+
+    private void AdjustDateTimeItem_Click(object sender, RoutedEventArgs e)
+    {
+        try
+        {
+            Process.Start(new ProcessStartInfo
+            {
+                FileName = "ms-settings:dateandtime",
+                UseShellExecute = true
+            });
+        }
+        catch (Exception ex)
+        {
+            Context.API.ShowMsg(
+                Context.API.GetTranslation("FlowBarPlugin_Clock_RightClickMenu_FailToOpenDateTime"),
+                ex.Message);
+        }
     }
 }
