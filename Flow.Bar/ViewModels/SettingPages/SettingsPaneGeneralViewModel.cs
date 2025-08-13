818047d6e20576b8d9a2e7aa46469676a1a02575
Jack251970 <1160210343@qq.com>
2025-08-13 16:04:00 +0800

    Add setting ui

--- Flow.Bar/ViewModels/SettingPages/SettingsPaneGeneralViewModel.cs
+++ Flow.Bar/ViewModels/SettingPages/SettingsPaneGeneralViewModel.cs
@@ -1,13 +1,19 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
 using CommunityToolkit.Mvvm.ComponentModel;
+using Flow.Bar.Helpers.Startup;
 using Flow.Bar.Models.Language;
 using Flow.Bar.Models.UserSettings;
+using Windows.Win32;
 
 namespace Flow.Bar.ViewModels;
 
 public partial class SettingsPaneGeneralViewModel(Settings settings, Internationalization translater) : ObservableObject
 {
     public Settings Settings { get; init; } = settings;
+
+    #region Language
 
     private readonly Internationalization _translater = translater;
 
@@ -18,4 +24,132 @@ public partial class SettingsPaneGeneralViewModel(Settings settings, Internation
         get => Settings.Language;
         set => _translater.ChangeLanguage(value);
     }
+
+    #endregion
+
+    #region Behaviour
+
+    public bool StartOnSystemStartup
+    {
+        get => Settings.StartOnSystemStartup;
+        set
+        {
+            if (StartOnSystemStartup == value) return;
+
+            Settings.StartOnSystemStartup = value;
+
+            try
+            {
+                if (value)
+                {
+                    if (UseLogonTaskForStartup)
+                    {
+                        AutoStartupHelper.ChangeToViaLogonTask(AlwaysRunAsAdministrator);
+                    }
+                    else
+                    {
+                        AutoStartupHelper.ChangeToViaRegistry();
+                    }
+                }
+                else
+                {
+                    AutoStartupHelper.DisableViaLogonTaskAndRegistry();
+                }
+            }
+            catch (Exception e)
+            {
+                App.API.ShowMsg(Localize.App_FailedToSetAutoStartup(), e.Message);
+            }
+
+            // If we have enabled logon task startup, we need to check if we need to restart the app
+            // even if we encounter an error while setting the startup method
+            if (value && UseLogonTaskForStartup)
+            {
+                CheckAdminChangeAndAskForRestart();
+            }
+        }
+    }
+
+    public bool UseLogonTaskForStartup
+    {
+        get => Settings.UseLogonTaskForStartup;
+        set
+        {
+            if (UseLogonTaskForStartup == value) return;
+
+            Settings.UseLogonTaskForStartup = value;
+
+            if (StartOnSystemStartup)
+            {
+                try
+                {
+                    if (value)
+                    {
+                        AutoStartupHelper.ChangeToViaLogonTask(AlwaysRunAsAdministrator);
+                    }
+                    else
+                    {
+                        AutoStartupHelper.ChangeToViaRegistry();
+                    }
+                }
+                catch (Exception e)
+                {
+                    App.API.ShowMsg(Localize.App_FailedToSetAutoStartup(), e.Message);
+                }
+            }
+
+            // If we have enabled logon task startup, we need to check if we need to restart the app
+            // even if we encounter an error while setting the startup method
+            if (StartOnSystemStartup && value)
+            {
+                CheckAdminChangeAndAskForRestart();
+            }
+        }
+    }
+
+    public bool AlwaysRunAsAdministrator
+    {
+        get => Settings.AlwaysRunAsAdministrator;
+        set
+        {
+            if (AlwaysRunAsAdministrator == value) return;
+
+            Settings.AlwaysRunAsAdministrator = value;
+
+            if (StartOnSystemStartup && UseLogonTaskForStartup)
+            {
+                try
+                {
+                    AutoStartupHelper.ChangeToViaLogonTask(value);
+                }
+                catch (Exception e)
+                {
+                    App.API.ShowMsg(Localize.App_FailedToSetAutoStartup(), e.Message);
+                }
+
+                // If we have enabled logon task startup, we need to check if we need to restart the app
+                // even if we encounter an error while setting the startup method
+                CheckAdminChangeAndAskForRestart();
+            }
+        }
+    }
+
+    private void CheckAdminChangeAndAskForRestart()
+    {
+        // When we change from non-admin to admin, we need to restart the app as administrator to apply the changes
+        // Under non-administrator, we cannot delete or set the logon task which is run as administrator
+        if (AlwaysRunAsAdministrator && !PInvokeHelper.IsAdministrator())
+        {
+            if (App.API.ShowMsgBox(
+                Localize.SettingPaneGeneral_RestartToApplyChange(),
+                Localize.SettingPaneGeneral_RestartToApplyChangeDescription(),
+                MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+            {
+                // Restart the app as administrator
+                App.API.RestartApp(true);
+            }
+        }
+    }
+
+    #endregion
 }
