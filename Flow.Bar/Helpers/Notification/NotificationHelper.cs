255761a1657e5d5db12905c14e9a1e1e2b254134
Jack251970 <1160210343@qq.com>
2025-08-13 17:41:37 +0800

    Improve NotificationHelper

--- Flow.Bar/Helpers/Notification/NotificationHelper.cs
+++ Flow.Bar/Helpers/Notification/NotificationHelper.cs
@@ -10,21 +10,21 @@ public class NotificationHelper
 
     public static void Show(string title, string subTitle, string? iconPath = null)
     {
-        var icon = File.Exists(iconPath) ? iconPath : null;
+        var icon = File.Exists(iconPath) ? iconPath : Constants.DefaultIcon;
 
         try
         {
             var builder = new ToastContentBuilder()
-                .AddText(title, hintMaxLines: 1);
-            if (!string.IsNullOrEmpty(subTitle))
-            {
-                builder.AddText(subTitle);
-            }
-            if (!string.IsNullOrEmpty(icon))
-            {
-                builder.AddAppLogoOverride(new Uri(icon));
-            }
+                .AddText(title ?? string.Empty, hintMaxLines: 1)
+                .AddText(subTitle ?? string.Empty)
+                .AddAppLogoOverride(new Uri(icon));
             builder.Show();
+        }
+        catch (InvalidOperationException e)
+        {
+            // Temporary fix for the Windows 11 notification issue
+            // Possibly from 22621.1413 or 22621.1485, judging by post time of #2024
+            App.API.LogFatal(ClassName, "Failed to show notification because of Windows system", e);
         }
         catch (Exception e)
         {
