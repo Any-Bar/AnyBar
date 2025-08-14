59f57b5a80e140dc39fa9d07d439d50bbc37f6bc
Jack251970 <1160210343@qq.com>
2025-08-14 22:40:35 +0800

    Add dependency property for left-click menu

--- Flow.Bar/Helpers/MenuFlyout/MenuFlyoutHelperDependencyProperty.cs
+++ Flow.Bar/Helpers/MenuFlyout/MenuFlyoutHelperDependencyProperty.cs
@@ -5,6 +5,30 @@ namespace Flow.Bar.Helpers.MenuFlyout;
 
 public class MenuFlyoutHelperDependencyProperty
 {
+    public static readonly DependencyProperty LeftClickMenuFlyoutHelperProperty =
+        DependencyProperty.RegisterAttached(
+            "LeftClickMenuFlyoutHelper",
+            typeof(AppBarMenuFlyoutHelper),
+            typeof(MenuFlyoutHelperDependencyProperty),
+            new PropertyMetadata(null));
+
+    [AttachedPropertyBrowsableForType(typeof(UIElement))]
+    public static AppBarMenuFlyoutHelper? GetLeftClickMenuFlyoutHelper(FrameworkElement element)
+    {
+        ArgumentNullException.ThrowIfNull(element);
+
+        return (AppBarMenuFlyoutHelper?)element.GetValue(LeftClickMenuFlyoutHelperProperty);
+    }
+
+    [AttachedPropertyBrowsableForType(typeof(UIElement))]
+    public static void SetLeftClickMenuFlyoutHelper(FrameworkElement element, AppBarMenuFlyoutHelper value)
+    {
+        ArgumentNullException.ThrowIfNull(element);
+        ArgumentNullException.ThrowIfNull(value);
+
+        element.SetValue(LeftClickMenuFlyoutHelperProperty, value);
+    }
+
     public static readonly DependencyProperty RightClickMenuFlyoutHelperProperty =
         DependencyProperty.RegisterAttached(
             "RightClickMenuFlyoutHelper",
