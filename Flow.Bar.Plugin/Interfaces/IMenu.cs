dbb9115bc0bc9b93807ff38a8878875a1b6a8899
Jack251970 <1160210343@qq.com>
2025-08-14 21:55:50 +0800

    Add IMenuBase

--- Flow.Bar.Plugin/Interfaces/IMenu.cs
+++ Flow.Bar.Plugin/Interfaces/IMenu.cs
@@ -11,7 +11,7 @@ namespace Flow.Bar.Plugin;
 /// Do not implement this interface directly.
 /// Use <see cref="ILeftClickMenu"/> or <see cref="IRightClickMenu"/> instead.
 /// </remarks>
-public interface IMenu
+public interface IMenu: IMenuBase
 {
     /// <summary>
     /// Retrieves a list of menu items for the context menu.
@@ -27,7 +27,7 @@ public interface IMenu
 /// Do not implement this interface directly.
 /// Use <see cref="ICustomLeftClickMenu"/> or <see cref="ICustomRightClickMenu"/> instead.
 /// </remarks>
-public interface ICustomMenu
+public interface ICustomMenu: IMenuBase
 {
     /// <summary>
     /// Retrieves style for the context menu.
@@ -39,4 +39,19 @@ public interface ICustomMenu
     /// Applies the template to the context menu.
     /// </summary>
     void OnApplyTemplate(ContextMenu menu);
+}
+
+/// <summary>
+/// Base interface for context menus.
+/// </summary>
+/// <remarks>
+/// Do not implement this interface directly.
+/// Use <see cref="ICustomLeftClickMenu"/> or <see cref="ICustomRightClickMenu"/> instead.
+/// </remarks>
+public interface IMenuBase
+{
+    /// <summary>
+    /// Retrieves the popup mode for the context menu.
+    /// </summary>
+    ContextMenuPopupMode PopupMode { get; }
 }
