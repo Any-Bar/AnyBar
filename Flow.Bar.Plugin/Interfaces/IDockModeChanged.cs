77275537093cfba7656c3f572e2f3ffe4e132923
Jack251970 <1160210343@qq.com>
2025-08-15 15:49:12 +0800

    Add IDockModeChanged

---
+++ Flow.Bar.Plugin/Interfaces/IDockModeChanged.cs
@@ -0,0 +1,16 @@
+﻿namespace Flow.Bar.Plugin.Interfaces;
+
+/// <summary>
+/// Interface for bar elements of Flow Bar plugins.
+/// </summary>
+/// <remarks>
+/// This interface should be implemented by bar elements returned from <see cref="IAsyncPlugin.GetBarElement(BarElementPosition)"/>.
+/// </remarks>
+public interface IDockModeChanged
+{
+    /// <summary>
+    /// Invoked when the dock mode changes.
+    /// </summary>
+    /// <param name="position"></param>
+    void OnDockModeChanged(BarElementPosition position);
+}
