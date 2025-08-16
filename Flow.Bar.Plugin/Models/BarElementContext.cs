4ecf1f66e9ca128c4df04eee7262d2a7f24cab71
Jack251970 <1160210343@qq.com>
2025-08-16 17:18:45 +0800

    Add BarElementContext

---
+++ Flow.Bar.Plugin/Models/BarElementContext.cs
@@ -0,0 +1,12 @@
+﻿namespace Flow.Bar.Plugin;
+
+/// <summary>
+/// Context for bar elements of Flow Bar plugins.
+/// </summary>
+public class BarElementContext
+{
+    /// <summary>
+    /// The unique identifier of the bar element.
+    /// </summary>
+    public string Id { get; }
+}
