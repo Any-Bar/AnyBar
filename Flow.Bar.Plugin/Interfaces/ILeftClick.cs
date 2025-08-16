07e2fad36ceade40ba46b76e4d59f75deb249769
Jack251970 <1160210343@qq.com>
2025-08-16 12:05:42 +0800

    Add ILeftClick & IRightClick

---
+++ Flow.Bar.Plugin/Interfaces/ILeftClick.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows.Input;
+
+namespace Flow.Bar.Plugin.Interfaces;
+
+/// <summary>
+/// Interface for Flow Bar plugins that have left-click events.
+/// </summary>
+public interface ILeftClick
+{
+    /// <summary>
+    /// Invoked when the left mouse button is pressed on the bar element.
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    void OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e);
+
+    /// <summary>
+    /// Invoked when the left mouse button is released on the bar element.
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    void OnMouseLeftButtonUp(object sender, MouseButtonEventArgs e);
+}
---
+++ Flow.Bar.Plugin/Interfaces/IRightClick.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows.Input;
+
+namespace Flow.Bar.Plugin.Interfaces;
+
+/// <summary>
+/// Interface for Flow Bar plugins that have right-click events.
+/// </summary>
+public interface IRightClick
+{
+    /// <summary>
+    /// Invoked when the right mouse button is pressed on the bar element.
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    void OnMouseRightButtonDown(object sender, MouseButtonEventArgs e);
+
+    /// <summary>
+    /// Invoked when the right mouse button is released on the bar element.
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    void OnMouseRightButtonUp(object sender, MouseButtonEventArgs e);
+}
