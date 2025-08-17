1fe1b7121d7e4ac9ff6a2af197b5f96033b9782b
Jack251970 <1160210343@qq.com>
2025-08-17 12:14:21 +0800

    Remove IEquatable for BarElementModel & Add PluginElementContext property

--- Flow.Bar/Models/AppBar/BarElementModel.cs
+++ Flow.Bar/Models/AppBar/BarElementModel.cs
@@ -1,17 +1,20 @@
-﻿using System;
-using System.Text.Json.Serialization;
+﻿using System.Text.Json.Serialization;
 using Flow.Bar.Enums;
 using Flow.Bar.Interfaces;
+using Flow.Bar.Plugin;
 
 namespace Flow.Bar.Models.AppBar;
 
-public class BarElementModel : IEquatable<BarElementModel>, IOrder
+public class BarElementModel : IOrder
 {
     [JsonIgnore]
     public AppBarModel AppBar { get; set; } = null!;
 
     [JsonIgnore]
     public BarElementModelPosition BarElementPosition { get; set; } = BarElementModelPosition.LeftOrTop;
+
+    [JsonIgnore]
+    public BarElementContext? Context { get; set; } = null;
 
     public int Order { get; set; } = -1;
 
@@ -21,35 +24,4 @@ public class BarElementModel : IEquatable<BarElementModel>, IOrder
     /// This name is used to display plugin name when the plugin is missing
     /// </summary>
     public string Name { get; set; } = string.Empty;
-
-    /// <inheritdoc />
-    public override bool Equals(object? obj)
-    {
-        return Equals(obj as BarElementModel);
-    }
-
-    /// <inheritdoc />
-    public override int GetHashCode() => AppBar.Order.GetHashCode() ^ Order.GetHashCode();
-
-    /// <inheritdoc />
-    public bool Equals(BarElementModel? other) => AppBar.Order == other?.AppBar.Order && Order == other?.Order;
-
-    /// <inheritdoc />
-    public static bool operator ==(BarElementModel? a, BarElementModel? b)
-    {
-        if (ReferenceEquals(a, b))
-        {
-            return true;
-        }
-
-        if (a is null)
-        {
-            return false;
-        }
-
-        return a.Equals(b);
-    }
-
-    /// <inheritdoc />
-    public static bool operator !=(BarElementModel? a, BarElementModel? b) => !(a == b);
 }
