ee8c2d63269f624619269fb6345dc22c001b0b28
Jack251970 <1160210343@qq.com>
2025-10-05 09:06:14 +0800

    Fix blank space

--- Localization/Flow.Bar.Localization.SourceGenerators/Localize/PublicApiSourceGenerator.cs
+++ Localization/Flow.Bar.Localization.SourceGenerators/Localize/PublicApiSourceGenerator.cs
@@ -88,7 +88,7 @@ namespace Flow.Bar.Localization.SourceGenerators.Localize
             sourceBuilder.AppendLine($"{tabString}/// <summary>");
             sourceBuilder.AppendLine($"{tabString}/// Get <see cref=\"Flow.Bar.Plugin.IPublicAPI\"> instance");
             sourceBuilder.AppendLine($"{tabString}/// </summary>");
-            sourceBuilder.AppendLine($"{tabString}internal static Flow.Bar.Plugin.IPublicAPI {Constants.PublicApiInternalPropertyName} =>" +
+            sourceBuilder.AppendLine($"{tabString}internal static Flow.Bar.Plugin.IPublicAPI {Constants.PublicApiInternalPropertyName} => " +
                 $"{Constants.PublicApiPrivatePropertyName} ??= CommunityToolkit.Mvvm.DependencyInjection.Ioc.Default.GetRequiredService<Flow.Bar.Plugin.IPublicAPI>();");
             sourceBuilder.AppendLine($"}}");
 
