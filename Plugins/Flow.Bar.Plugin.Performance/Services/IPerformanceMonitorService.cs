1afd39ecdf17ee74909a817d23eeba0135c93b94
Jack251970 <1160210343@qq.com>
2025-10-06 10:25:38 +0800

    Add Flow.Bar.Plugin.Performance

---
+++ Plugins/Flow.Bar.Plugin.Performance/Services/IPerformanceMonitorService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Flow.Bar.Plugin.Performance.Services;
+
+public interface IPerformanceMonitorService
+{
+    event EventHandler<PerformanceDataEventArgs> PerformanceDataUpdated;
+
+    void StartMonitoring();
+
+    void StopMonitoring();
+
+    PerformanceData GetCurrentPerformanceData();
+}
+
+public class PerformanceDataEventArgs : EventArgs
+{
+    public PerformanceData Data { get; }
+    
+    public PerformanceDataEventArgs(PerformanceData data)
+    {
+        Data = data;
+    }
+}
+
+public record PerformanceData(
+    double CpuUsage,
+    double MemoryUsage, 
+    double? GpuUsage
+);
